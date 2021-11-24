012c6759bd9d3cd5b4a6394cccfaa4b696dd0c91
Leha <maltsev0017@gmail.com>
2021-11-24 15:14:31 +0300

    Homework for lesson 5 is done

--- src/main/java/MainActivity.java
+++ src/main/java/MainActivity.java
@@ -5,52 +5,50 @@ import java.util.HashMap;
 
 /*
 Task:
-1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
-Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
-Посчитать, сколько раз встречается каждое слово.
-2. Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров.
-В этот телефонный справочник с помощью метода add() можно добавлять записи,
-а с помощью метода get() искать номер телефона по фамилии. Следует учесть, что под одной фамилией
-может быть несколько телефонов (в случае однофамильцев), тогда при запросе такой фамилии должны выводиться все телефоны.
-Желательно не добавлять лишний функционал (дополнительные поля (имя, отчество, адрес), взаимодействие с пользователем
-через консоль и т.д). Консоль использовать только для вывода результатов проверки телефонного справочника.
+Необходимо написать два метода, которые делают следующее:
+1) Создают одномерный длинный массив, например:
+static final int SIZE = 10_000_000;
+static final int HALF = size / 2;
+float[] arr = new float[size];
+
+2) Заполняют этот массив единицами.
+3) Засекают время выполнения: long a = System.currentTimeMillis().
+4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
+arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+5) Проверяется время окончания метода System.currentTimeMillis().
+6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a).
+
  */
 
 public class MainActivity {
-    static PhoneNumberList phoneNumberList = new PhoneNumberList();
-    static String[] stringArray = {
-            "Hello",
-            "world",
-            "We",
-            "world",
-            "live",
-            "in",
-            "Hello"
-    };
-    static ArrayList<String> uniqueArray = new ArrayList<>();
-    static HashMap<String, Integer> quantityOfValue = new HashMap<>();
-    public static void main(String[] args) {
-        for(int i = 0; i < stringArray.length; i++) {
-            if(quantityOfValue.containsKey(stringArray[i])) {
-                quantityOfValue.put(stringArray[i],quantityOfValue.get(stringArray[i]) + 1);
-            } else {
-                quantityOfValue.put(stringArray[i], 1);
-                uniqueArray.add(stringArray[i]);
-            }
-        }
-        System.out.println("Уникальные слова:");
-        System.out.println(uniqueArray);
-        System.out.println("Всего слов в массиве");
-        System.out.println(quantityOfValue);
-        System.out.println("Первое задание выполнено");
-        System.out.println("____________________________________________________________________________");
-        System.out.println("");
+    static final int SIZE = 10000;
+    static final int TEST_SIZE = 10;
+    static long currentTimeMillis;
+    static double[] array = new double[SIZE];
 
-        System.out.println("Второе задание");
-        phoneNumberList.add("key1", "3388");
-        phoneNumberList.add("key2", "4477");
-        phoneNumberList.add("key3", "5599");
-        phoneNumberList.add("key1", "6660");
-        System.out.println(phoneNumberList.get("key1"));
+    public static void main(String[] args) {
+        initArray();
+        changeArrayValue();
+        printArray();
+        System.out.println("Время работы программы:");
+        System.out.println(System.currentTimeMillis() - currentTimeMillis);
+    }
+
+    static void initArray() {
+        currentTimeMillis = System.currentTimeMillis();
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = 1;
+        }
+    }
+
+    static void changeArrayValue() {
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = (double) (array[i] * Math.sin(0.2 + i / 5) * Math.cos(0.2 + i / 5) * Math.cos(0.4 + i / 2));
+        }
+    }
+    static void printArray() {
+        for (int i = 0; i < TEST_SIZE; i++) {
+            System.out.println(i + 1 + ": " + array[i]);
+        }
     }
 }
